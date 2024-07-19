9eb0dbd55e34cc07f6a84133069cb43af05fc54b
de123456sdf <cdaeun95@gmail.com>
2024-07-19 12:49:51 +0900

    Revert "Revert "add: 어플리케이션 추가""
    This reverts commit a05e8e41823a97e6afe730c20d994522687e07ad.

---
+++ src/main/java/cmtproject/comento/ComentoApplication.java
@@ -0,0 +1,13 @@
+package cmtproject.comento;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ComentoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ComentoApplication.class, args);
+    }
+
+}
