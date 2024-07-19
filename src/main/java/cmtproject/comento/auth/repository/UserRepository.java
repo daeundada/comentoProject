2d497f2fda43f32bc832de4a283489228272d0f6
de123456sdf <cdaeun95@gmail.com>
2024-07-19 12:49:02 +0900

    Revert "feature: 로그인, 회원가입 api 구현"
    This reverts commit 85c57869eb01210cdf2d2b2b8a7c114ceef8e9eb.

--- src/main/java/cmtproject/comento/auth/repository/UserRepository.java
+++
@@ -1,9 +0,0 @@
-package cmtproject.comento.auth.repository;
-
-import cmtproject.comento.auth.entity.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserRepository extends JpaRepository<User, Long> {
-
-    User findByUsername(String username);
-}
